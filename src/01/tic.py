228793fba6696f7e292deb94174467f6c4a82f7e
Lymond Glenda <lymondgl@ge-f6.kzn.21-school.ru>
2022-08-03 13:17:20 +0300

    part_1 is ready

---
+++ src/01/tic.py
@@ -0,0 +1,12 @@
+
+def print_X_O():
+    """This functtion output X and O"""
+    for i in range(3):
+        if i == 0:
+            print("XOX")
+        elif i == 1:
+            print("OXO")
+        else:
+            print("XXO")
+
+print_X_O()
