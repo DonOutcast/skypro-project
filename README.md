dcc91ef38789e394461a83644a93ff90a7d8fb39
Lymond Glenda <lymondgl@ge-f3.kzn.21-school.ru>
2022-08-05 11:06:14 +0300

    part_3 is ready

--- README.md
+++ README.md
@@ -57,3 +57,97 @@ The greater-than symbol followed by a space (> ) represents the user input. Note
 | _ _ X |
 | _ _ _ |
 ---------
+
+## Part 3 What's up on the field?
+### Description
+In this stage, we're going to analyze the game state to determine if either player has already won the game or it is still ongoing, if the game is a draw, or if the user has entered an impossible game state (two winners, or with one player having made too many moves).
+
+### Objectives
+In this stage, your program should:
+
+Take a string entered by the user and print the game grid as in the previous stage.
+Analyze the game state and print the result. Possible states:
+Game not finished when neither side has three in a row but the grid still has empty cells.
+Draw when no side has a three in a row and the grid has no empty cells.
+X wins when the grid has three X's in a row (including diagonals).
+O wins when the grid has three O's in a row (including diagonals).
+Impossible when the grid has three X's in a row as well as three O's in a row, or there are a lot more X's than O's or vice versa (the difference should be 1 or 0; if the difference is 2 or more, then the game state is impossible).
+In this stage, we will assume that either X or O can start the game.
+
+You can choose whether to use a space or underscore _ to print empty cells.
+
+### Examples
+The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
+
+- Example 1:
+
+> XXXOO__O_
+---------
+| X X X |
+| O O _ |
+| _ O _ |
+---------
+X wins
+- Example 2:
+
+> XOXOXOXXO
+---------
+| X O X |
+| O X O |
+| X X O |
+---------
+X wins
+- Example 3:
+
+> XOOOXOXXO
+---------
+| X O O |
+| O X O |
+| X X O |
+---------
+O wins
+- Example 4:
+
+> XOXOOXXXO
+---------
+| X O X |
+| O O X |
+| X X O |
+---------
+Draw
+- Example 5:
+
+> XO_OOX_X_
+---------
+| X O   |
+| O O X |
+|   X   |
+---------
+Game not finished
+- Example 6:
+
+> XO_XO_XOX
+---------
+| X O _ |
+| X O _ |
+| X O X |
+---------
+Impossible
+- Example 7:
+
+> _O_X__X_X
+---------
+|   O   |
+| X     |
+| X   X |
+---------
+Impossible
+- Example 8:
+
+> _OOOO_X_X
+---------
+|   O O |
+| O O   |
+| X   X |
+---------
+Impossible
--- src/03/tic.py
+++ src/03/tic.py
@@ -1,70 +1,28 @@
 user_input = input()
-
-def convert_to_list(stroka: str) -> list:
-    """This function coverting your string to list"""
-    spisok = []
-    spisok_a = ''
-    spisok_b = ''
-    spisok_c = ''
-    spisok_a = user_input[0:3]
-    # if user_input[0:1] == 'X' and user_input[1:2] == 'X' and user_input[2:3] == "X":
-    #     print("win")
-    # elif user_input[0:1] == "X" and user_input[4:5] == "X" and user_input[8:9] == "X":
-    #     print("Maan")
-    # elif user_input[0:1] == "X" and user_input[3:4] == "X" and user_input[6:7] == "X":
-    #     print("EE")
-    # elif user_input[]
-    spisok_b = user_input[3:6]
-    spisok_c = user_input[6:9]
-    spisok.append(spisok_a)
-    spisok.append(spisok_b)
-    spisok.append(spisok_c)
-    return spisok
+array = [char if char in "XO" else "_" for char in user_input]
 
 
-def print_grid(spisok: list) -> None:
-    """This function print to console grid """
-    print("---------")
-    for rows in spisok:
-        print("| ", " ".join(words for words in rows), " |")
-    print("---------")
+def print_grid(stroka: str) -> None:
+    """This is function print your answer in grid game."""
+    print("-" * 9 + "\n| {} {} {} |\n| {} {} {} |\n| {} {} {} |\n".format(*stroka) + "-" * 9)
+
+
 
 def o_wins(spisok: list) -> None:
-    if spisok[0][0:1] == 'O' and spisok[0][1:2] == "O" and spisok[0][2:3] == "O":
-        print("O wins")
-    elif spisok[1][0:1] == 'O' and spisok[1][1:2] == "O" and spisok[1][2:3] == "O":
-        print("O wins")
-    elif spisok[2][0:1] == 'O' and spisok[2][1:2] == 'O' and spisok[2][2:3] == "O":
-        print("O wins")
-    elif spisok[0][0:1] == 'O' and spisok[1][1:2] == 'O' and spisok[2][2:3] == 'O':
-        print("O wins")
-    elif spisok[0][2:3] == 'O' and spisok[1][1:2] == 'O' and spisok[2][0:1] == 'O':
-        print("O wins")
-    elif spisok[0][0:1] == 'O' and spisok[1][0:1] == 'O' and spisok[2][0:1] == 'O':
-        print("O wins")
-    elif spisok[0][1:2] == "O" and spisok[1][1:2] == 'O' and spisok[2][1:2] == 'O':
-        print("O wins")
-    elif spisok[0][2:3] == 'O' and spisok[1][2:3] == 'O' and spisok[2][2:3] == 'O':
-        print("O wins")
-
-
-def x_wins(spisok: list) -> None:
-    if spisok[0][0:1] == 'X' and spisok[0][1:2] == "X" and spisok[0][2:3] == "X":
+    """This is function print how win and some rules in the game."""
+    possible_wins = [spisok[0] + spisok[1] + spisok[2], spisok[3] + spisok[4] + spisok[5], spisok[6] + spisok[7] + spisok[8],
+                     spisok[0] + spisok[3] + spisok[6], spisok[1] + spisok[4] + spisok[7], spisok[2] + spisok[5] + spisok[8],
+                     spisok[0] + spisok[4] + spisok[8], spisok[2] + spisok[4] + spisok[6]]
+    if 'X' * 3 in possible_wins and 'O' * 3 not in possible_wins:
         print("X wins")
-    elif spisok[1][0:1] == 'X' and spisok[1][1:2] == "X" and spisok[1][2:3] == "X":
-        print("X wins")
-    elif spisok[2][0:1] == 'X' and spisok[2][1:2] == 'X' and spisok[2][2:3] == "X":
-        print("X wins")
-    elif spisok[0][0:1] == 'X' and spisok[1][1:2] == 'X' and spisok[2][2:3] == 'X':
-        print("X wins")
-    elif spisok[0][2:3] == 'X' and spisok[1][1:2] == 'X' and spisok[2][0:1] == 'X':
-        print("X wins")
-    elif spisok[0][0:1] == 'X' and spisok[1][0:1] == 'X' and spisok[2][0:1] == 'X':
-        print("X wins")
-    elif spisok[0][1:2] == "X" and spisok[1][1:2] == 'X' and spisok[2][1:2] == 'X':
-        print("X wins")
-    elif spisok[0][2:3] == 'X' and spisok[1][2:3] == 'X' and spisok[2][2:3] == 'X':
-        print("X wins")
-
-a = convert_to_list(user_input)
-x_wins(a)
+    elif 'O' * 3 in possible_wins and 'X' * 3 not in possible_wins:
+        print("O wins")
+    elif "_" not in array:
+        print("Draw")
+    elif '_' in array and 'X' * 3 not in possible_wins and 'O' * 3 not in possible_wins and array.count("X") == array.count("O"):
+        print("Game not finished")
+    else:
+        print("Impossible")
+        
+print_grid(array)
+o_wins(array)
